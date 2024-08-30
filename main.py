60bca8b854f5b93e8ed7eafc97a53b927a12a30e
Eppo <eppesuig08@gmail.com>
2024-08-30 20:07:04 +0200

    Added GUI input system

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
+from PIL import Image, ImageTk
 import tkinter as tk
 import requests
 import json
 
-window = tk.Tk()
+def cerca_anime(animeInputEntry):
+  animeInput = animeInputEntry.get()
+  get_anime_info(animeInput=animeInput)
 
-window.geometry("600x600")
-window.title("AnimeAhead!")
-window.resizable(False, False)
+def get_anime_info(animeInput):
+  try:
+    url = f"https://api.jikan.moe/v4/anime?q={animeInput}"
 
-animeInput = input("Inserisci l'anime di cui vuoi avere informazioni: ")
+    response = requests.get(url)
+    animeInfo = response.json()
 
-url = f"https://api.jikan.moe/v4/anime?q={animeInput}"
+    nomeAnime = animeInfo["data"][0]["title_english"]
+    nEpisodi = animeInfo["data"][0]["episodes"]
+    studio = animeInfo["data"][0]["studios"][0]["name"]
+    voto = animeInfo["data"][0]["score"]
+    trama = animeInfo["data"][0]["synopsis"]
 
-try:
-  response = requests.get(url)
-  animeInfo = response.json()
+    if nomeAnime == None:
+      print(f"\nNome: {animeInput}")
+    else:
+      print(f"\nNome: {nomeAnime}")
+    print(f"N. episodi: {nEpisodi}")
+    print(f"Studio: {studio}")
+    print(f"Voto: {voto}")
+    print(f"Trama:\n{trama}\n")
+  except:
+    print(f'Non abbiamo trovato niente inerente a "{animeInput}" :(')
+  
+def main():
+  window = tk.Tk()
 
-  nomeAnime = animeInfo["data"][0]["title_english"]
-  nEpisodi = animeInfo["data"][0]["episodes"]
-  studio = animeInfo["data"][0]["studios"][0]["name"]
-  voto = animeInfo["data"][0]["score"]
-  trama = animeInfo["data"][0]["synopsis"]
+  window.geometry("600x600")
+  window.title("AnimeAhead!")
+  window.resizable(False, False)
+  tk.Label(window, text="Inserisci qui sotto l'anime di cui vuoi avere informazioni").pack(pady=20)
 
-  if nomeAnime == None:
-    print(f"\nNome: {animeInput}")
-  else:
-    print(f"\nNome: {nomeAnime}")
-  print(f"N. episodi: {nEpisodi}")
-  print(f"Studio: {studio}")
-  print(f"Voto: {voto}")
-  print(f"Trama:\n{trama}\n")
-except:
-  print(f'Non abbiamo trovato niente inerente a "{animeInput}" :(')
+  animeInputEntry = tk.Entry(window, width=40)
+  animeInputEntry.pack(pady=5)
+
+  button = tk.Button(window, text="Invio", command=lambda: cerca_anime(animeInputEntry)).pack(pady=5)
+
+  window.mainloop()
+
+
+if __name__ == "__main__":
+  main()
